aa7b13e01249c25698be9d628da89390871a5d83
Kamal <63669390+agrkamal@users.noreply.github.com>
2021-06-30 21:51:47 +0530

    remove log call

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,3 @@
-console.log('Java Script File');
-
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
 const messageOne = document.querySelector('#error')
